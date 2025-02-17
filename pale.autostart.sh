f41ab9f91c9b59c6d9aa68b99c62d221a882f55d
Pawel Lisewski <dev@palekiwi.com>
2025-02-17 17:24:27 +0800

    update autostart

--- pale.autostart.sh
+++ pale.autostart.sh
@@ -7,28 +7,9 @@ function run {
   fi
 }
 
-function run_flatpak {
-  if ! flatpak ps | grep $1;
-  then
-    flatpak run $@&
-  fi
-}
-
-function run_app {
-  if ! pgrep $1;
-  then
-    ~/apps/$@&
-  fi
-}
-
-run picom -b --config $HOME/.config/picom/picom.conf
-run sxhkd
 run ibus-daemon -drxR
-run unclutter --timeout 1 --start-hidden --ignore-scrolling
 
 run slack
 run nextcloud
 
 xmodmap ~/.Xmodmap
-
-run xscreensaver -no-splash
