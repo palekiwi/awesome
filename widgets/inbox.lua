f0763f9d1966367a3b9780081ba2480fa4cd0578
Pawel Lisewski <dev@palekiwi.com>
2025-05-09 22:06:48 +0800

    add opts

--- widgets/inbox.lua
+++ widgets/inbox.lua
@@ -4,20 +4,40 @@ local gears = require("gears")
 local inbox = {}
 
 
-function inbox.create()
-  local HOME = os.getenv("HOME")
+local function count_files(dir)
+  local count = 0
+  local cmd = "find '" .. dir .. "' -maxdepth 1 -type f | wc -l"
+  local handle = io.popen(cmd)
 
+  if handle then
+    count = tonumber(handle:read("*a")) or 0
+    handle:close()
+  end
+
+  return count
+end
+
+function inbox.create(opts)
+  opts = opts or {}
+
+  local HOME = os.getenv("HOME")
+  local directory = opts.dir or os.getenv("HOME") .. "/Maildir/ygt/Inbox/new"
   local fg_color = "#1d1d1f"
   local bg_color = "#ffbd7a"
   local radius = 2
 
-  local text = "3"
+  local text = "0"
   local icon_path = HOME .. "/.nix-profile/share/icons/Arc/status/symbolic/mail-unread-symbolic.svg"
 
   local text_widget = wibox.widget {
     markup = '<span weight="bold" foreground="' .. fg_color .. '">' .. text .. '</span>',
     widget = wibox.widget.textbox
   }
+
+  local function update_count()
+    local count = count_files(directory)
+    text_widget.markup = '<span weight="bold" foreground="' .. fg_color .. '">' .. count .. '</span>'
+  end
 
   local icon_widget = wibox.widget {
     image = gears.color.recolor_image(icon_path, fg_color),
@@ -48,6 +68,14 @@ function inbox.create()
       gears.shape.rounded_rect(cr, width, height, radius)
     end,
     widget = wibox.container.background
+  }
+
+  update_count()
+
+  gears.timer {
+    timeout = 30,
+    autostart = true,
+    callback = update_count
   }
 
   return wibox.widget {
