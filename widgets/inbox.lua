3c539907327b3f7e9e58eecfa17549d22b50cda2
Pawel Lisewski <dev@palekiwi.com>
2025-05-09 22:18:22 +0800

    Update colors

--- widgets/inbox.lua
+++ widgets/inbox.lua
@@ -22,25 +22,22 @@ function inbox.create(opts)
 
   local HOME = os.getenv("HOME")
   local directory = opts.dir or os.getenv("HOME") .. "/Maildir/ygt/Inbox/new"
-  local fg_color = "#1d1d1f"
-  local bg_color = "#ffbd7a"
+  local fg_color_active = "#1d1d1f"
+  local fg_color_inactive = "#1d1d1f"
+  local bg_color_active = "#ffbd7a"
+  local bg_color_inactive = "#666"
   local radius = 2
 
   local text = "0"
   local icon_path = HOME .. "/.nix-profile/share/icons/Arc/status/symbolic/mail-unread-symbolic.svg"
 
   local text_widget = wibox.widget {
-    markup = '<span weight="bold" foreground="' .. fg_color .. '">' .. text .. '</span>',
+    markup = '<span weight="bold" foreground="' .. fg_color_active .. '">' .. text .. '</span>',
     widget = wibox.widget.textbox
   }
 
-  local function update_count()
-    local count = count_files(directory)
-    text_widget.markup = '<span weight="bold" foreground="' .. fg_color .. '">' .. count .. '</span>'
-  end
-
   local icon_widget = wibox.widget {
-    image = gears.color.recolor_image(icon_path, fg_color),
+    image = gears.color.recolor_image(icon_path, fg_color_active),
     resize = true,
     widget = wibox.widget.imagebox
   }
@@ -63,12 +60,30 @@ function inbox.create(opts)
 
   local inbox_widget = wibox.widget {
     padded_widget,
-    bg = bg_color,
+    bg = bg_color_active,
     shape = function(cr, width, height)
       gears.shape.rounded_rect(cr, width, height, radius)
     end,
     widget = wibox.container.background
   }
+
+  local function update_count()
+    local count = count_files(directory)
+    local fg_color = fg_color_inactive
+    local bg_color = bg_color_inactive
+
+    if count > 0 then
+      fg_color = fg_color_active
+      bg_color = bg_color_active
+    end
+
+    local icon = gears.color.recolor_image(icon_path, fg_color)
+
+    text_widget.markup = '<span weight="bold" foreground="' .. fg_color .. '">' .. count .. '</span>'
+    inbox_widget.bg = bg_color
+    icon_widget.image = icon
+  end
+
 
   update_count()
 
