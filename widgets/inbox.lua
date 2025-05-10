030e63067359ac21688fe255edffa1ffa2986355
Pawel Lisewski <dev@palekiwi.com>
2025-05-10 22:28:29 +0800

    Count with notmuch

--- widgets/inbox.lua
+++ widgets/inbox.lua
@@ -1,12 +1,34 @@
 local wibox = require("wibox")
 local gears = require("gears")
 
+require("widgets.config")
+
+---@class InboxWidget.Colors
+---@field fg_active string
+---@field bg_active string
+---@field fg_inactive string
+---@field bg_inactive string
+
+---@class InboxWidget.Opts
+---@field colors? InboxWidget.Colors
+---@field dir? string
+---@field term string
+---@field timeout? number
+
 local inbox = {}
 
+local default_colors = {
+  fg_active   = "#1d1d1f",
+  bg_active   = "#ffbd7a",
+  fg_inactive = "#1d1d1f",
+  bg_inactive = "#666666"
+}
 
-local function count_files(dir)
+---@param term string
+---@return number
+local function count_by(term)
   local count = 0
-  local cmd = "find '" .. dir .. "' -maxdepth 1 -type f | wc -l"
+  local cmd = "notmuch count " .. term
   local handle = io.popen(cmd)
 
   if handle then
@@ -17,27 +39,34 @@ local function count_files(dir)
   return count
 end
 
+---@param opts InboxWidget.Opts
 function inbox.create(opts)
   opts = opts or {}
+  local colors = opts.colors or default_colors
+  local timeout = opts.timeout or 15
+  local term = 'tag:unread AND path:"ygt/Inbox/**"'
 
   local HOME = os.getenv("HOME")
-  local directory = opts.dir or os.getenv("HOME") .. "/Maildir/ygt/Inbox/new"
-  local fg_color_active = "#1d1d1f"
-  local fg_color_inactive = "#1d1d1f"
-  local bg_color_active = "#ffbd7a"
-  local bg_color_inactive = "#666"
   local radius = 2
 
-  local text = "0"
   local icon_path = HOME .. "/.nix-profile/share/icons/Arc/status/symbolic/mail-unread-symbolic.svg"
 
+  ---@param text? string
+  ---@param fg_color? string
+  local function span(text, fg_color)
+    text = text or "0"
+    fg_color = fg_color or colors.fg_inactive
+
+    return '<span weight="bold" foreground="' .. fg_color .. '">' .. text .. '</span>'
+  end
+
   local text_widget = wibox.widget {
-    markup = '<span weight="bold" foreground="' .. fg_color_active .. '">' .. text .. '</span>',
+    markup = span(),
     widget = wibox.widget.textbox
   }
 
   local icon_widget = wibox.widget {
-    image = gears.color.recolor_image(icon_path, fg_color_active),
+    image = gears.color.recolor_image(icon_path, colors.fg_active),
     resize = true,
     widget = wibox.widget.imagebox
   }
@@ -60,48 +89,40 @@ function inbox.create(opts)
 
   local inbox_widget = wibox.widget {
     padded_widget,
-    bg = bg_color_active,
+    bg = colors.bg_active,
     shape = function(cr, width, height)
       gears.shape.rounded_rect(cr, width, height, radius)
     end,
     widget = wibox.container.background
   }
 
-  local function update_count()
-    local count = count_files(directory)
-    local fg_color = fg_color_inactive
-    local bg_color = bg_color_inactive
+  local function update()
+    local count = count_by(term)
+    local active = count > 0
 
-    if count > 0 then
-      fg_color = fg_color_active
-      bg_color = bg_color_active
-    end
+    local fg_color = active and colors.fg_active or colors.fg_inactive
+    local bg_color = active and colors.bg_active or colors.bg_inactive
 
     local icon = gears.color.recolor_image(icon_path, fg_color)
 
-    text_widget.markup = '<span weight="bold" foreground="' .. fg_color .. '">' .. count .. '</span>'
+    text_widget.markup = span(tostring(count), fg_color)
     inbox_widget.bg = bg_color
     icon_widget.image = icon
   end
 
-
-  update_count()
+  update()
 
   gears.timer {
-    timeout = 30,
+    timeout = timeout,
     autostart = true,
-    callback = update_count
+    callback = update
   }
 
   return wibox.widget {
     inbox_widget,
-    left = 3,
-    right = 3,
-    top = 3,
-    bottom = 3,
+    margins = MARGINS,
     widget = wibox.container.margin
   }
 end
-
 
 return inbox
